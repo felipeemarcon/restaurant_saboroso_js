2bf20a2255e1104c9282cebab1c1fc6352a1485f
Felipe Marcon <felipeemarcon@gmail.com>
2019-01-10 23:24:58 -0200

    Organize Header with includes

--- routes/index.js
+++ routes/index.js
@@ -20,19 +20,35 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/contacts', function(req, res, next) {
-  res.render('contacts', { title: 'Contato - Restaurante Sababoro' });
+  res.render('contacts', { 
+    title: 'Contato - Restaurante Sababoro',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga olá!'
+  });
 });
 
 router.get('/menus', function(req, res, next) {
-  res.render('menus', { title: 'Menu - Restaurante Sababoro' });
+  res.render('menus', {
+    title: 'Menu - Restaurante Sababoro',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!'
+  });
 });
 
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Serviços - Restaurante Sababoro' });
+  res.render('services', {
+    title: 'Serviços - Restaurante Sababoro',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
+  });
 });
 
 router.get('/reservations', function(req, res, next) {
-  res.render('reservations', { title: 'Reservas - Restaurante Sababoro' });
+  res.render('reservations', {
+    title: 'Reservas - Restaurante Sababoro',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma Mesa!'
+  });
 });
 
 module.exports = router;
